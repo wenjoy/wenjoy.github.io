a233c7962f20bcce6d47ac2db2ed831277030d5d
wenjun <laojiaodaoniu@126.com>
2022-07-07 10:38:14 +0800

    fix: get ws url dynamicly

--- socket/index.ts
+++ socket/index.ts
@@ -5,39 +5,36 @@ interface socket {
 
 let ws: socket
 
-const url = 'ws://localhost:8888'
-
 export async function getSocket(): Promise<socket> {
   if (ws) {
     return ws
   }
 
   return fetch('/api/socket').then(() => {
-    // ws = new WebSocket('ws://localhost:3000/api/socket');
     return new Promise((res, rej) => {
-
-      const connect = new WebSocket(url);
+      const url = `ws://${location.hostname}:8888`
+      const connect = new WebSocket(url)
 
       connect.onopen = function () {
-        console.log('connected');
+        console.log('connected')
         ws = {
           send(data) {
             connect.send(JSON.stringify(data))
           },
           listen(msg, handler) {
             connect.addEventListener(msg, handler)
-          }
+          },
         }
 
         res(ws)
       }
 
       connect.onmessage = function (event) {
-        console.log('message is: ', event.data);
+        console.log('message is: ', event.data)
       }
 
       connect.onclose = function (event) {
-        console.log('connection closed', event);
+        console.log('connection closed', event)
         rej(Error('websocket has been closed'))
       }
     })
